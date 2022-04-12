c677ed7bb01dd6390a9f05b4d7d9c5454c2970f2
杨玉峰 <yangyufeng@reyun.com>
2022-04-12 14:54:26 +0800

    增加示例展示

--- examples/app.vue
+++ examples/app.vue
@@ -1,10 +1,60 @@
 <template>
-  <div>
-    事例：请通过router进行访问
-    <router-view></router-view>
+  <div class="app">
+    <div class="coms">
+      <p>点击按钮查看组件示例</p>
+      <Button v-for="(item, i) in comsList"
+              class="com-btn"
+              :class="{active: activeRoute === item.path}"
+              :key="i"
+              @click="$router.push(item.path)">{{item.name}}</Button>
+    </div>
+    <div class="view">
+      <p class="result-desc">示例结果如下：</p>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      comsList: [],
+      activeRoute: ''
+    };
+  },
+  created() {
+    const { routes } = this.$router.options;
+    this.comsList = routes.map(r => {
+      const path = r.path;
+      const name = path.slice(1);
+      return { name, path };
+    });
+  },
+  watch: {
+    $route: {
+      deep: true,
+      immediate: true,
+      handler() {
+        this.activeRoute = this.$router.currentRoute.path;
+      }
+    }
+  }
+};
 </script>
+<style lang="less" scoped>
+.coms {
+  margin: 16px;
+  .com-btn {
+    margin-top: 16px;
+    margin-right: 16px;
+  }
+}
+.view {
+  margin: 16px;
+  border-top: 1px dashed #ddd;
+  .result-desc {
+    margin: 16px 0;
+  }
+}
+</style>
