7db682a6fd798fc22e4917661bd7750f27d17882
杨玉峰 <yangyufeng@reyun.com>
2022-04-12 10:50:21 +0800

    页头组件代码简化

--- src/components/basics/page-header/page-header.vue
+++ src/components/basics/page-header/page-header.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: yangyufeng
  * @Date: 2022-04-07 14:19:18
- * @LastEditTime: 2022-04-11 15:20:42
+ * @LastEditTime: 2022-04-12 10:49:20
  * @LastEditors: Please set LastEditors
  * @Description: 页头组件
  * @FilePath: /ry-design/src/components/basics/page-header/page-header.vue
@@ -9,79 +9,49 @@
 <template>
   <div :class="classes">
     <!-- 面包屑 -->
-    <div v-if="isPropBreadcrumb"
+    <div v-if="isPropBreadcrumb || isHasSolt('breadcrumb')"
          :class="[prefixCls + '-breadcrumb']">
-      <rd-breadcrumb :data="breadcrumb">
-      </rd-breadcrumb>
+      <slot name="breadcrumb">
+        <rd-breadcrumb :data="breadcrumb">
+        </rd-breadcrumb>
+      </slot>
     </div>
-    <!-- 插槽面包屑 -->
-    <template v-else-if="isHasSolt('breadcrumb')">
-      <div :class="[prefixCls + '-breadcrumb']">
-        <slot name="breadcrumb"></slot>
-      </div>
-    </template>
     <!-- 头部 -->
     <div :class="[prefixCls + '-heading']">
       <!-- 左侧 -->
       <div :class="[prefixCls + '-heading-left']">
         <!-- 返回按钮 -->
-        <div v-if="backIcon"
+        <div v-if="backIcon || isHasSolt('backIcon')"
              :class="prefixCls + '-back'">
           <span class="back-icon"
                 @click="back($event)">
-            <svg focusable="false"
-                 class="back-icon-img"
-                 data-icon="arrow-left"
-                 width="1em"
-                 height="1em"
-                 fill="currentColor"
-                 aria-hidden="true"
-                 viewBox="64 64 896 896">
-              <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 000 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"></path>
-            </svg>
+            <slot name="backIcon">
+              <svg width="1em"
+                   height="1em"
+                   fill="currentColor"
+                   viewBox="64 64 896 896">
+                <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 000 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"></path>
+              </svg>
+            </slot>
           </span>
         </div>
-        <!-- 插槽返回按钮 -->
-        <template v-else-if="isHasSolt('backIcon')">
-          <div :class="prefixCls + '-back'">
-            <span class="back-icon">
-              <slot name="backIcon"></slot>
-            </span>
-          </div>
-        </template>
         <!-- 头像 -->
-        <span v-if="isPropAvatar"
+        <span v-if="isPropAvatar || isHasSolt('avatar')"
               :class="[prefixCls + '-avatar']">
-          <img :src="avatar" />
+          <slot name="avatar">
+            <img :src="avatar" />
+          </slot>
         </span>
-        <!-- 插槽头像 -->
-        <template v-else-if="isHasSolt('avatar')">
-          <span :class="[prefixCls + '-avatar']">
-            <slot name="avatar"></slot>
-          </span>
-        </template>
         <!-- 标题 -->
-        <span v-if="title"
+        <span v-if="title || isHasSolt('title')"
               :class="[prefixCls + '-heading-title']">
-          {{ title }}
+          <slot name="title">{{ title }}</slot>
         </span>
-        <!-- 插槽标题 -->
-        <template v-else-if="isHasSolt('title')">
-          <span :class="[prefixCls + '-heading-title']">
-            <slot name="title"></slot>
-          </span>
-        </template>
         <!-- 副标题 -->
-        <span v-if="subTitle"
+        <span v-if="subTitle || isHasSolt('subTitle')"
               :class="[prefixCls + '-heading-sub-title']">
-          {{ subTitle }}
+          <slot name="subTitle">{{ subTitle }}</slot>
         </span>
-        <!-- 插槽副标题 -->
-        <template v-else-if="isHasSolt('subTitle')">
-          <span :class="[prefixCls + '-heading-sub-title']">
-            <slot name="subTitle"></slot>
-          </span>
-        </template>
         <!-- 插槽标签 -->
         <span v-if="isHasSolt('tags')"
               :class="[prefixCls + '-heading-tags']">
