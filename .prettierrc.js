0f4a000abf25cc6f26365e354c2bf0c6efa8db66
周博 <zhoubo@reyun.com>
2022-04-29 15:02:03 +0800

    eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  'prettier.jsxBracketSameLine': false,
+  htmlWhitespaceSensitivity: 'ignore',
+  bracketSameLine: true,
+  singleAttributePerLine: true,
+  // 箭头函数参数括号
+  arrowParens: 'avoid'
+};
