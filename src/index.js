c7f0052940630571c58ab4bd2e299d2fc5fd41fd
杨玉峰 <yangyufeng@reyun.com>
2022-05-15 17:12:05 +0800

    新增组件单行出价编辑

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 // basics
 import Transfer from './components/basics/transfer'
-import MultiCascader from './components/basics/multi-cascader/index.js'
-import PrefixContainer from './components/basics/prefix-container/index.js'
-import SingleHeader from './components/basics/single-header/index.js'
-import PageHeader from './components/basics/page-header/index.js'
-import Breadcrumb from './components/basics/breadcrumb/index.js'
-import MultiRadioButton from './components/basics/multi-radio-button/index.js'
-import Modals from './components/basics/modals/index.js'
+import MultiCascader from './components/basics/multi-cascader'
+import PrefixContainer from './components/basics/prefix-container'
+import SingleHeader from './components/basics/single-header'
+import PageHeader from './components/basics/page-header'
+import Breadcrumb from './components/basics/breadcrumb'
+import MultiRadioButton from './components/basics/multi-radio-button'
+import Modals from './components/basics/modals'
+import RadioGroup from './components/basics/radio-group'
 // business
-import PageHeaderModule from './components/business/page-header-module/index.js'
-import AccountList from './components/business/account-list/index.js'
-import RadioGroup from './components/basics/radio-group/index.js'
+import PageHeaderModule from './components/business/page-header-module'
+import AccountList from './components/business/account-list'
+import EditRowBid from './components/business/edit-row-bid'
 
 import ViewUI from 'view-design'
 import Package from '../package.json'
@@ -39,10 +40,11 @@ const components = [
   AccountList,
   PageHeaderModule,
   RadioGroup,
-  Modals
+  Modals,
+  EditRowBid
 ]
 
-const install = function (Vue) {
+const install = function(Vue) {
   components.forEach(component => {
     Vue.component(component.name, component)
   })
@@ -63,5 +65,6 @@ export default {
   Breadcrumb,
   MultiRadioButton,
   PageHeaderModule,
-  Modals
+  Modals,
+  EditRowBid
 }
