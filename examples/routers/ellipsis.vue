1269d5249dcf93a083969190e5d791d81e505806
赵涛 <zhaotao@reyun.com>
2022-06-12 13:18:55 +0800

    表格使用示例

--- examples/routers/ellipsis.vue
+++ examples/routers/ellipsis.vue
@@ -42,6 +42,21 @@
           type="md-copy" />
       </rd-ellipsis>
     </div>
+    <h2>示例5-表格中使用</h2>
+    <Table
+      :columns="column"
+      :data="body"
+      border>
+      <template
+        slot="backup"
+        slot-scope="{ row }">
+        <rd-ellipsis
+          :text="row.backup"
+          :lines="2"
+          tooltip
+          transfer></rd-ellipsis>
+      </template>
+    </Table>
   </div>
 </template>
 
@@ -51,7 +66,27 @@ export default {
   props: {},
   data() {
     return {
-      text: '史蒂夫·乔布斯（英语：Steve Jobs），是一名美国企业家、营销家和发明家，苹果公司的联合创始人之一，曾任董事长及首席执行官职位，NeXT创办人及首席执行官，也是皮克斯动画的创办人并曾任首席执行官，2006年为华特迪士尼公司的董事会成员。2017年9月苹果发布会举行的地方，以他命名为 Steve Jobs Theater 。'
+      text: '史蒂夫·乔布斯（英语：Steve Jobs），是一名美国企业家、营销家和发明家，苹果公司的联合创始人之一，曾任董事长及首席执行官职位，NeXT创办人及首席执行官，也是皮克斯动画的创办人并曾任首席执行官，2006年为华特迪士尼公司的董事会成员。2017年9月苹果发布会举行的地方，以他命名为 Steve Jobs Theater 。',
+      column: [
+        {
+          key: 'name',
+          title: '姓名',
+          width: 100
+        },
+        {
+          key: 'backup',
+          title: '备注',
+          slot: 'backup',
+          width: 200
+        }
+      ],
+      body: [
+        {
+          name: '张胜男',
+          backup:
+            '史蒂夫·乔布斯（英语：Steve Jobs），是一名美国企业家、营销家和发明家，苹果公司的联合创始人之一，曾任董事长及首席执行官职位，NeXT创办人及首席执行官，也是皮克斯动画的创办人并曾任首席执行官'
+        }
+      ]
     }
   },
   computed: {},
