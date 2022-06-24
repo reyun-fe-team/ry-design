92d8d68e518f5d0efbaf66847af971a053afc7e9
杨玉峰 <yangyufeng@reyun.com>
2022-06-24 19:18:53 +0800

    feat：新增组件  text-item-show
    commit:
    
    footer：

--- examples/routers/text-item-show.vue
+++ examples/routers/text-item-show.vue
@@ -2,12 +2,67 @@
  * @Author: 杨玉峰 yangyufeng@reyun.com
  * @Date: 2022-06-15 19:53:40
  * @LastEditors: 杨玉峰 yangyufeng@reyun.com
- * @LastEditTime: 2022-06-15 19:56:17
+ * @LastEditTime: 2022-06-24 19:16:41
  * @FilePath: /ry-design/examples/routers/text-item-show.vue
  * @Description: 极速创建基础信息，单项显示控件 示例
 -->
 <template>
   <div style="margin: 20px">
-    <rd-text-item-show></rd-text-item-show>
+    <rd-text-item-show
+      two-line-display
+      :data="basic">
+      <template #btnGroup>
+        <Icon
+          style="width: 50px"
+          type="md-create" />
+      </template>
+    </rd-text-item-show>
+
+    <rd-text-item-show
+      type="title"
+      :data="title">
+      <template #btnGroup>
+        <Icon
+          style="width: 50px"
+          type="md-create" />
+      </template>
+    </rd-text-item-show>
+
+    <rd-text-item-show
+      type="classify"
+      :data="classify">
+      <template #btnGroup>
+        <Icon
+          style="width: 50px"
+          type="md-create" />
+      </template>
+    </rd-text-item-show>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      basic: {
+        text: '极速创建基础信息，单项显示控件 示例极速创建基础信息，单项显示控件 示例极速创建基础信息，单项显示控件 示例'
+      },
+      title: {
+        title: '极速创建基础信息',
+        subTitle: '单项显示控件 示例极速创建基础信息，单项显示控件 示例'
+      },
+      classify: {
+        classify: {
+          name: '商品库名称',
+          id: '5533567'
+        },
+        info: {
+          name: '商品名称',
+          id: '5533567'
+        },
+        thumbnail:
+          'https://t7.baidu.com/it/u=138126325,1485620701&fm=218&app=126&size=f242,150&n=0&f=JPEG&fmt=auto?s=7FAB2EC3909A35D01E299C1A030010D2&sec=1656176400&t=2db15a0e3ebd7bde9bff89d831d1633f'
+      }
+    }
+  }
+}
+</script>
--- src/components/business/text-item-show/text-item-show.vue
+++ src/components/business/text-item-show/text-item-show.vue
@@ -2,12 +2,64 @@
  * @Author: 杨玉峰 yangyufeng@reyun.com
  * @Date: 2022-06-15 19:43:38
  * @LastEditors: 杨玉峰 yangyufeng@reyun.com
- * @LastEditTime: 2022-06-15 19:51:17
+ * @LastEditTime: 2022-06-24 19:18:26
  * @FilePath: /ry-design/src/components/business/text-item-show/text-item-show.vue
  * @Description: 极速创建基础信息，单项显示控件
 -->
 <template>
-  <main></main>
+  <div :class="[prefixCls]">
+    <!-- basic -->
+    <div
+      v-if="type === 'basic'"
+      :class="[prefixCls + '-basic', { 'two-lines': twoLineDisplay }]">
+      <div :class="[prefixCls + '-basic-text', { 'two-line-text': twoLineDisplay }, 'show-text']">
+        {{ data.text }}
+      </div>
+      <!-- btnGroup -->
+      <div :class="[prefixCls + '-btns']">
+        <slot name="btnGroup"></slot>
+      </div>
+    </div>
+
+    <!-- title -->
+    <div
+      v-if="type === 'title'"
+      :class="[prefixCls + '-title']">
+      <div :class="[prefixCls + '-title-text', 'show-text']">
+        <div class="main-title">{{ data.title }}</div>
+        <div class="sub-title">{{ data.subTitle }}</div>
+      </div>
+      <!-- btnGroup -->
+      <div :class="[prefixCls + '-btns']">
+        <slot name="btnGroup"></slot>
+      </div>
+    </div>
+
+    <!-- classify -->
+    <div
+      v-if="type === 'classify'"
+      :class="[prefixCls + '-classify']">
+      <div :class="[prefixCls + '-classify-text', 'show-text']">
+        <div class="classify-thumbnail">
+          <img :src="data.thumbnail" />
+        </div>
+        <div class="classify-infos">
+          <div class="classify-info-item">
+            <span class="classify-item-name">{{ data.classify.name }}</span>
+            <span class="classify-item-id">（ID：{{ data.classify.id }}）</span>
+          </div>
+          <div class="classify-info-item">
+            <span class="classify-item-name">{{ data.info.name }}</span>
+            <span class="classify-item-id">（ID：{{ data.info.id }}）</span>
+          </div>
+        </div>
+      </div>
+      <!-- btnGroup -->
+      <div :class="[prefixCls + '-btns']">
+        <slot name="btnGroup"></slot>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 const { prefix } = require('../../../config.js')
@@ -26,6 +78,29 @@ export default {
       default: false
     },
     // 显示的数据
+    // basic
+    // {
+    //   text: '最短信息长度超过25个字—最短信息长度超过25个字'
+    // }
+
+    // title
+    // {
+    //   title: '标题&主要信息',
+    //   subTitle: '次要信息、ID、出'
+    // }
+
+    // classify
+    // {
+    //   classify: {
+    //     name: '商品库名称',
+    //     id: '5533567'
+    //   },
+    //   info: {
+    //     name: '商品名称',
+    //     id: '5533567'
+    //   },
+    //   thumbnail: require('./product.png')
+    // }
     data: {
       type: Object,
       default: () => ({})
