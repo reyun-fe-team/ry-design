a3dc3660f0d62078cb0003fac6058efaa337533e
MuYinfu <muyinfu@163.com>
2022-09-21 14:18:24 +0800

    feat: radio-group组件添加最小宽度

--- examples/routers/radio-group.vue
+++ examples/routers/radio-group.vue
@@ -16,7 +16,7 @@ export default {
       value: '111',
       list: [
         {
-          label: '按展示付费(oCPM)',
+          label: '按',
           value: 'PRICING_OCPM',
           disabledTooltip: '123123123',
           tooltip: ['11111111111111', '22222222'],
