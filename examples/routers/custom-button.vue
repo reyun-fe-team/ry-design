7db8dc1cfc4a20b4be06990d269217fc1e901fdf
杨玉峰 <yangyufeng@mobvista.com>
2022-12-07 18:25:58 +0800

    feat: 常规按钮展示完成

--- examples/routers/custom-button.vue
+++ examples/routers/custom-button.vue
@@ -1,11 +1,71 @@
 <template>
   <div>
-    <rd-button
-      type="dashed"
-      disabled
-      icon="ios-search">
-      测试
-    </rd-button>
+    <div style="margin-bottom: 25px">
+      <h3 style="margin-bottom: 15px">常规按钮</h3>
+      <div style="margin-bottom: 10px; padding-left: 10px">
+        <span style="display: inline-block; width: 100px">主要按钮</span>
+        <span>
+          <rd-button
+            type="primary"
+            icon="ios-search">
+            正常状态
+          </rd-button>
+          <rd-button
+            type="primary"
+            loading>
+            加载中状态
+          </rd-button>
+          <rd-button
+            type="primary"
+            disabled
+            icon="ios-search">
+            禁用状态
+          </rd-button>
+        </span>
+      </div>
+      <div style="margin-bottom: 10px; padding-left: 10px">
+        <span style="display: inline-block; width: 100px">默认纯白按钮</span>
+        <span>
+          <rd-button
+            type="default"
+            icon="ios-search">
+            正常状态
+          </rd-button>
+          <rd-button
+            type="default"
+            loading>
+            加载中状态
+          </rd-button>
+          <rd-button
+            type="default"
+            disabled
+            icon="ios-search">
+            禁用状态
+          </rd-button>
+        </span>
+      </div>
+      <div style="margin-bottom: 10px; padding-left: 10px">
+        <span style="display: inline-block; width: 100px">灰框按钮</span>
+        <span>
+          <rd-button
+            type="dashed"
+            icon="ios-search">
+            正常状态
+          </rd-button>
+          <rd-button
+            type="dashed"
+            loading>
+            加载中状态
+          </rd-button>
+          <rd-button
+            type="dashed"
+            disabled
+            icon="ios-search">
+            禁用状态
+          </rd-button>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
