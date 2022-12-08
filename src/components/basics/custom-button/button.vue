0bacb5be8ad48c750939b6ee6b6dc35396baae35
杨玉峰 <yangyufeng@mobvista.com>
2022-12-08 16:11:52 +0800

    feat: 插画按钮完成

--- src/components/basics/custom-button/button.vue
+++ src/components/basics/custom-button/button.vue
@@ -1,12 +1,17 @@
 <!--
  * @Date: 2022-12-07 16:13:28
- * @LastEditTime: 2022-12-07 19:42:50
+ * @LastEditTime: 2022-12-08 15:37:20
  * @Description: 按钮组件
 -->
 <template>
   <div
     :class="classes"
     @click="handleClick">
+    <!-- 角标 -->
+    <div
+      v-if="cornerMarker"
+      class="recommend"
+      :style="cornerMarker"></div>
     <!--普通按钮 -->
     <normalized-button
       v-if="isNormalized"
@@ -19,10 +24,11 @@
     <!-- 插画按钮 -->
     <illustration-button
       v-if="type === 'illustration'"
-      :loading="loading"
       :disabled="disabled"
       :illustration-url="illustrationUrl"
-      :corner-marker="cornerMarker">
+      :is-drop-down="isDropDown"
+      :drop-down-items="dropDownItems"
+      :drop-down-props="dropDownProps">
       <slot></slot>
     </illustration-button>
   </div>
@@ -41,6 +47,29 @@ export default {
     illustrationButton
   },
   props: {
+    // 下拉属性（插画按钮支持）
+    dropDownItems: {
+      type: Array,
+      default: () => []
+    },
+    // 开启下拉（插画按钮支持）
+    isDropDown: {
+      type: Boolean,
+      default: false
+    },
+    // 插画图片地址（插画按钮支持）
+    illustrationUrl: {
+      type: String,
+      default: ''
+    },
+    // Dropdown Props,下拉参数选项
+    dropDownProps: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    // ------------------通用----------------
     // 组件类型
     // 普通按钮 text 文字按钮 primary 主要按钮  dashed 灰色框按钮 default 默认纯白色按钮
     // 特殊按钮 icon 图标按钮 illustration 插画按钮
@@ -50,11 +79,6 @@ export default {
     },
     // icon字体类型
     icon: {
-      type: String,
-      default: ''
-    },
-    // 插画图片地址
-    illustrationUrl: {
       type: String,
       default: ''
     },
--- src/components/basics/custom-button/illustration-button.vue
+++ src/components/basics/custom-button/illustration-button.vue
@@ -1,39 +1,59 @@
 <!--
  * @Date: 2022-12-07 18:30:20
- * @LastEditTime: 2022-12-07 20:07:31
+ * @LastEditTime: 2022-12-08 15:40:33
  * @Description: 插画按钮
 -->
 <template>
-  <div :class="classes">
-    <button
-      class="illustration-button"
-      :disabled="disabled"></button>
-    <div class="illustration-background">
-      <div
-        class="head"
-        :style="headIcon"></div>
-      <div class="text">
+  <Dropdown
+    trigger="hover"
+    v-bind="dropDownProps">
+    <Button
+      type="primary"
+      :class="[btnClass]"
+      :disabled="disabled">
+      <!-- 文本 -->
+      <span class="illustration-text">
         <slot></slot>
-      </div>
+      </span>
+      <!-- 插画图 -->
       <div
-        v-if="cornerMarkerIcon"
-        class="recommend"
-        :style="cornerMarkerIcon"></div>
-    </div>
-  </div>
+        class="illustration-image"
+        :style="headIcon"></div>
+      <!-- 下拉箭头 -->
+      <Icon
+        v-if="showDropdownMenu"
+        type="ios-arrow-down"
+        class="illustration-arrow"></Icon>
+    </Button>
+    <template
+      v-if="showDropdownMenu"
+      #list>
+      <DropdownMenu>
+        <DropdownItem
+          v-for="item in dropDownItems"
+          :key="item.name"
+          v-bind="getDropdownItemProps(item)">
+          <Render
+            v-if="item.render"
+            :render="item.render"></Render>
+          <span v-else>{{ item.label }}</span>
+        </DropdownItem>
+      </DropdownMenu>
+    </template>
+  </Dropdown>
 </template>
 <script>
 import { prefix } from '@src/config.js'
 const prefixCls = prefix + 'button-illustration'
+import Render from './../../base/render'
+import _isArray from 'lodash/isArray'
 
 export default {
   name: 'ButtonIllustration',
+  components: {
+    Render
+  },
   props: {
-    // 加载中状态
-    loading: {
-      type: Boolean,
-      default: false
-    },
     // 是否禁用按钮
     disabled: {
       type: Boolean,
@@ -44,29 +64,57 @@ export default {
       type: String,
       default: ''
     },
-    // 按钮角标（右上角）
-    // eslint-disable-next-line vue/require-default-prop
-    // styles property
-    cornerMarker: {
-      type: Object
+    // 下拉属性（插画按钮支持）
+    // DropdownItem Props
+    // 额外参数： label 文本， render 自定义渲染内容
+    dropDownItems: {
+      type: Array,
+      default: () => []
+    },
+    // 下拉参数选项（插画按钮支持）
+    // Dropdown Props
+    dropDownProps: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    // 开启下拉（插画按钮支持）
+    isDropDown: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
     // 类名class集合
-    classes() {
-      const classList = [`${prefixCls}`, 'illustration']
-      return classList
+    btnClass() {
+      return `${prefixCls}`
     },
     headIcon() {
       return {
-        'background-position': 'left center',
-        'background-repeat': 'no-repeat',
-        'background-image': `url(${this.illustrationUrl})`,
-        'background-size': 'contain'
+        'background-image': `url(${this.illustrationUrl})`
       }
     },
-    cornerMarkerIcon() {
-      return this.cornerMarker
+    showDropdownMenu() {
+      if (!this.isDropDown || this.disabled) {
+        return false
+      }
+      if (_isArray(this.dropDownItems) && this.dropDownItems.length > 0) {
+        return true
+      }
+      return false
+    }
+  },
+  methods: {
+    getDropdownItemProps(item) {
+      // name	用来标识这一项	String	-
+      // disabled	禁用该项	Boolean	false
+      // divided	显示分割线	Boolean	false
+      // selected	标记该项为选中状态	Boolean	false
+      const list = ['name', 'disabled', 'divided', 'selected']
+      let data = {}
+      list.forEach(k => (data[k] = item[k]))
+      return data
     }
   }
 }
