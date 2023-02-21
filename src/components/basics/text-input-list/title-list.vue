4ea579bbeeaa85a85a7245f3859023842b7782a5
傅文 <fuwen@reyun.com>
2023-02-21 14:47:38 +0800

    feat: validFn方法返回当前的索引。

--- src/components/basics/text-input-list/title-list.vue
+++ src/components/basics/text-input-list/title-list.vue
@@ -347,7 +347,7 @@ export default {
         errors.push('lengthError')
       }
       if (typeof this.propsValidFn === 'function') {
-        const allErrors = this.propsValidFn(value)
+        const allErrors = this.propsValidFn(value, this.index)
         errors = [...errors, ...allErrors]
       }
       return errors
