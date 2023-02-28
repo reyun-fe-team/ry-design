979b50621d3bfe34df1b16dec4baf6eba3b82d29
MuYinfu <muyinfu@163.com>
2023-02-28 15:22:05 +0800

    feat:  添加时间选择器 组件

---
+++ src/components/basics/date-picker/data.js
@@ -0,0 +1,244 @@
+export const shortcutsList = [
+  '0day',
+  '-1day',
+  '-7day',
+  '-30day',
+  '0week',
+  '-1week',
+  '0month',
+  '-1month'
+]
+
+export const getShortcutsOptionsList = (start, self) => [
+  {
+    id: '-1day',
+    text: '昨天',
+    value() {
+      const date = new Date()
+      date.setTime(date.getTime() - 3600 * 1000 * 24)
+      return [date, date]
+    }
+  },
+  {
+    id: '0day',
+    text: '今天',
+    value() {
+      return [new Date(), new Date()]
+    }
+  },
+  {
+    id: '-1week',
+    text: '上周',
+    value() {
+      // 获取当前时间
+      var currentDate = new Date()
+      // 返回date是一周中的某一天
+      var week = currentDate.getDay()
+      // 一天的毫秒数
+      var millisecond = 1000 * 60 * 60 * 24
+      // 减去的天数
+      var minusDay = week !== 0 ? week - 1 : 6
+      // 获得当前周的第一天
+      var currentWeekDayOne = new Date(currentDate.getTime() - millisecond * minusDay)
+      // 上周最后一天即本周开始的前一天
+      var priorWeekLastDay = new Date(currentWeekDayOne.getTime() - millisecond)
+      // 上周的第一天
+      var priorWeekFirstDay = new Date(priorWeekLastDay.getTime() - millisecond * 6)
+      return [priorWeekFirstDay, priorWeekLastDay]
+    }
+  },
+  {
+    id: '0week',
+    text: '本周',
+    value() {
+      // 获取当前时间
+      var currentDate = new Date()
+      // 返回date是一周中的某一天
+      var week = currentDate.getDay()
+      // 一天的毫秒数
+      var millisecond = 1000 * 60 * 60 * 24
+      // 减去的天数
+      var minusDay = week !== 0 ? week - 1 : 6
+      // 本周 周一
+      var monday = new Date(currentDate.getTime() - minusDay * millisecond)
+      // 本周 周日
+      var sunday = new Date(monday.getTime() + minusDay * millisecond)
+      return [monday, sunday]
+    }
+  },
+  {
+    id: '-1month',
+    text: '上月',
+    value() {
+      // 获取当前时间
+      var currentDate = new Date()
+      // 获得当前月份0-11
+      var currentMonth = currentDate.getMonth()
+      // 获得当前年份4位年
+      var currentYear = currentDate.getFullYear()
+      // 获得上一个月的第一天
+      var priorMonthFirstDay = self.getPriorMonthFirstDay(currentYear, currentMonth)
+      // 获得上一月的最后一天
+      var priorMonthLastDay = new Date(
+        priorMonthFirstDay.getFullYear(),
+        priorMonthFirstDay.getMonth(),
+        self.getMonthDays(priorMonthFirstDay.getFullYear(), priorMonthFirstDay.getMonth())
+      )
+      // 返回
+      return [priorMonthFirstDay, priorMonthLastDay]
+    }
+  },
+  {
+    id: '0month',
+    text: '本月',
+    value() {
+      // 起止日期数组
+      // 获取当前时间
+      var currentDate = new Date()
+      // 获得当前月份0-11
+      var currentMonth = currentDate.getMonth()
+      // 获得当前年份4位年
+      var currentYear = currentDate.getFullYear()
+      // 求出本月第一天
+      var firstDay = new Date(currentYear, currentMonth, 1)
+      return [firstDay, new Date()]
+    }
+  },
+  {
+    id: '-1quarter',
+    text: '上季度',
+    value() {
+      // 起止日期数组
+      // 获取当前时间
+      var currentDate = new Date()
+      // 获得当前月份0-11
+      var currentMonth = currentDate.getMonth()
+      // 获得当前年份4位年
+      var currentYear = currentDate.getFullYear()
+      // 获得本月对应的季度
+      var quarterSeasonStartMonth = self.getQuarterSeasonStartMonth(currentMonth)
+      // 上季度的第一天
+      var priorSeasonFirstDay = self.getPriorSeasonFirstDay(currentYear, quarterSeasonStartMonth)
+      // 上季度的最后一天
+      var priorSeasonLastDay = new Date(
+        priorSeasonFirstDay.getFullYear(),
+        priorSeasonFirstDay.getMonth() + 2,
+        self.getMonthDays(priorSeasonFirstDay.getFullYear(), priorSeasonFirstDay.getMonth() + 2)
+      )
+      return [priorSeasonFirstDay, priorSeasonLastDay]
+    }
+  },
+  {
+    id: '0quarter',
+    text: '本季度',
+    value() {
+      // 起止日期数组
+      // 获取当前时间
+      var currentDate = new Date()
+      // 获得当前月份0-11
+      var currentMonth = currentDate.getMonth()
+      // 获得当前年份4位年
+      var currentYear = currentDate.getFullYear()
+      // 获得本季度开始月份
+      var quarterSeasonStartMonth = self.getQuarterSeasonStartMonth(currentMonth)
+
+      // 获得本季度开始的日期
+      var quarterSeasonStartDate = new Date(currentYear, quarterSeasonStartMonth, 1)
+      // 添加至数组
+      return [quarterSeasonStartDate, new Date()]
+    }
+  },
+  {
+    id: '-1year',
+    text: '去年',
+    value() {
+      // 获取当前时间
+      var currentDate = new Date()
+      // 获得当前年份4位年
+      var currentYear = currentDate.getFullYear()
+      currentYear--
+      var priorYearFirstDay = new Date(currentYear, 0, 1)
+      var priorYearLastDay = new Date(currentYear, 11, 31)
+      return [priorYearFirstDay, priorYearLastDay]
+    }
+  },
+  {
+    id: '0year',
+    text: '今年',
+    value() {
+      // 获取当前时间
+      var currentDate = new Date()
+      // 获得当前年份4位年
+      var currentYear = currentDate.getFullYear()
+      // 本年第一天
+      var currentYearFirstDate = new Date(currentYear, 0, 1)
+      // 添加至数组
+      return [currentYearFirstDate, new Date()]
+    }
+  },
+  {
+    id: '-3day',
+    text: '近3天',
+    value() {
+      let date1 = new Date()
+      date1.setTime(date1.getTime() - 2 * 3600 * 1000 * 24)
+      if (start && new Date(start) > date1) {
+        date1 = new Date(new Date(start).getTime() + 3600 * 1000 * 24)
+      }
+      const date = new Date()
+      return [date1, date]
+    }
+  },
+  {
+    id: '-7day',
+    text: '近7天',
+    value() {
+      let date1 = new Date()
+      date1.setTime(date1.getTime() - 6 * 3600 * 1000 * 24)
+      if (start && new Date(start) > date1) {
+        date1 = new Date(new Date(start).getTime() + 3600 * 1000 * 24)
+      }
+      const date = new Date()
+      return [date1, date]
+    }
+  },
+  {
+    id: '-14day',
+    text: '近14天',
+    value() {
+      let date1 = new Date()
+      date1.setTime(date1.getTime() - 13 * 3600 * 1000 * 24)
+      if (start && new Date(start) > date1) {
+        date1 = new Date(new Date(start).getTime() + 3600 * 1000 * 24)
+      }
+      const date = new Date()
+      return [date1, date]
+    }
+  },
+  {
+    id: '-30day',
+    text: '近30天',
+    value() {
+      const date = new Date()
+      let date1 = new Date()
+      date1.setTime(date1.getTime() - 29 * 3600 * 1000 * 24)
+      if (start && new Date(start) > date1) {
+        date1 = new Date(new Date(start).getTime() + 3600 * 1000 * 24)
+      }
+      return [date1, date]
+    }
+  },
+  {
+    id: '-93day',
+    text: '近93天',
+    value() {
+      const date = new Date()
+      let date1 = new Date()
+      date1.setTime(date1.getTime() - 92 * 3600 * 1000 * 24)
+      if (start && new Date(start) > date1) {
+        date1 = new Date(new Date(start).getTime() + 3600 * 1000 * 24)
+      }
+      return [date1, date]
+    }
+  }
+]
--- src/components/basics/date-picker/date-picker.vue
+++ src/components/basics/date-picker/date-picker.vue
@@ -1,11 +1,290 @@
 <template>
-  <div>111</div>
+  <div :class="[prefixCls]">
+    <DatePicker
+      ref="rd-date-picker-inner"
+      v-model="selDate"
+      :transfer="transfer"
+      :confirm="confirm"
+      :type="type"
+      :clearable="clearable"
+      :options="getDateOptions"
+      :format="format"
+      :placement="placement"
+      :placeholder="placeholder"
+      :size="size"
+      @click.native="nativeClick"
+      @on-open-change="handleOpenChange"
+      @on-change="handleChange"
+      @on-ok="handleOk"
+      @on-clear="handleClear"
+      @on-clickoutside="handleClickoutside"></DatePicker>
+  </div>
 </template>
 
 <script>
 import { prefix } from '@src/config.js'
 const prefixCls = prefix + 'date-picker'
+import { getShortcutsOptionsList, shortcutsList } from './data'
 export default {
-  name: prefixCls
+  name: prefixCls,
+  props: {
+    value: {
+      default: () => []
+    },
+    transfer: {
+      type: Boolean,
+      default: false
+    },
+    confirm: {
+      type: Boolean,
+      default: true
+    },
+    // 左侧快捷方式
+    shortcuts: {
+      type: Array,
+      default: () => []
+    },
+    type: {
+      type: String,
+      default: 'date'
+    },
+    clearable: {
+      default: true
+    },
+    // 格式化日期展现形式
+    format: {
+      type: String,
+      default: 'yyyy-MM-dd'
+    },
+    // 弹窗出现位置
+    placement: {
+      type: String,
+      default: 'bottom-end'
+    },
+    // 提示文案
+    placeholder: {
+      type: String,
+      default: '选择日期'
+    },
+    size: {
+      type: String
+    },
+    start: {
+      type: String
+    },
+    end: {
+      type: String
+    },
+    limit: {
+      type: String
+    },
+    // 是否显示快捷操作项
+    showShortcuts: {
+      type: Boolean,
+      default: true
+    },
+    options: {
+      type: Object,
+      default: () => {}
+    },
+    startDate: {
+      type: Date
+    }
+  },
+  data() {
+    return {
+      prefixCls: prefixCls,
+      selDate: this.$props.value
+    }
+  },
+  computed: {
+    // 获取基础配置
+    getDateOptions({ options }) {
+      let self = this
+      let { start } = self
+
+      let shortcuts = []
+
+      // 是否开启快捷操作
+      if (this.showShortcuts) {
+        const optionsList = getShortcutsOptionsList(start, self)
+
+        // 左侧快速选择功能，传入参数优先级高于默认操作列
+        let _shortcuts = this.shortcuts && this.shortcuts.length ? this.shortcuts : shortcutsList
+
+        optionsList.forEach(item => {
+          let { id } = item
+          if (_shortcuts.indexOf(id) !== -1) {
+            shortcuts.push(item)
+          }
+        })
+      }
+
+      // 禁选按钮
+      let disabledDate = date => {
+        var config = false
+        if (this.limit) {
+          config = config || date < new Date(this.limit)
+        }
+        if (this.start) {
+          config = new Date(this.start) > date
+        }
+        if (this.end) {
+          config = config || date > new Date(this.end)
+        }
+        return config
+      }
+      return {
+        disabledDate,
+        shortcuts,
+        ...options
+      }
+    }
+  },
+  watch: {
+    value: function (value) {
+      this.selDate = value
+    }
+  },
+  methods: {
+    // 返回上一个月的第一天Date类型
+    getPriorMonthFirstDay(year, month) {
+      // 年份为0代表,是本年的第一月,所以不能减
+      if (month === 0) {
+        month = 11 // 月份为上年的最后月份
+        year-- // 年份减1
+        return new Date(year, month, 1)
+      }
+      // 否则,只减去月份
+      month--
+      return new Date(year, month, 1)
+    },
+    // 获得该月的天数
+    getMonthDays(year, month) {
+      // 本月第一天 1-31
+      var relativeDate = new Date(year, month, 1)
+      // 获得当前月份0-11
+      var relativeMonth = relativeDate.getMonth()
+      // 获得当前年份4位年
+      var relativeYear = relativeDate.getFullYear()
+      // 当为12月的时候年份需要加1
+      // 月份需要更新为0 也就是下一年的第一个月
+      if (relativeMonth === 11) {
+        relativeYear++
+        relativeMonth = 0
+      } else {
+        // 否则只是月份增加,以便求的下一月的第一天
+        relativeMonth++
+      }
+      // 一天的毫秒数
+      var millisecond = 1000 * 60 * 60 * 24
+      // 下月的第一天
+      var nextMonthDayOne = new Date(relativeYear, relativeMonth, 1)
+      // 返回得到上月的最后一天,也就是本月总天数
+      return new Date(nextMonthDayOne.getTime() - millisecond).getDate()
+    },
+    // 得到上季度的起始日期
+    getPriorSeasonFirstDay(year, month) {
+      // 春
+      var spring = 0
+      // 夏
+      var summer = 3
+      // 秋
+      var fall = 6
+      // 冬
+      var winter = 9
+      // 月份从0-11
+      // 季度的其实月份
+      switch (month) {
+        case spring:
+          // 如果是第一季度则应该到去年的冬季
+          year--
+          month = winter
+          break
+        case summer:
+          month = spring
+          break
+        case fall:
+          month = summer
+          break
+        case winter:
+          month = fall
+          break
+      }
+      return new Date(year, month, 1)
+    },
+    // 得到本季度开始的月份
+    getQuarterSeasonStartMonth(month) {
+      // 春
+      var spring = 0
+      // 夏
+      var summer = 3
+      // 秋
+      var fall = 6
+      // 冬
+      var winter = 9
+      // 月份从0-11
+      if (month < 3) {
+        return spring
+      }
+      if (month < 6) {
+        return summer
+      }
+      if (month < 9) {
+        return fall
+      }
+      return winter
+    },
+    // 弹出日历和关闭日历时触发
+    handleOpenChange() {},
+    // 日期发生变化时触发
+    handleChange(date) {
+      if (new Date(this.start) > new Date(date[0]) || new Date(this.end) < new Date(date[1])) {
+        this.$Message.info('时间范围超出，请确认')
+        return false
+      }
+      this.selDate = date
+      if (!this.confirm) {
+        this.$emit('input', this.selDate)
+        this.$emit('on-ok', this.selDate)
+      }
+    },
+    // 在 confirm 模式下有效，点击确定按钮时触发
+    handleOk() {
+      let isLength = !this.selDate.length
+      let isInvalid = this.selDate[0] === 'Invalid date' || this.selDate[1] === 'Invalid date'
+      let isEmpty = this.selDate[0] === '' || this.selDate[1] === ''
+
+      if (isLength || isInvalid || isEmpty) {
+        this.$Message.info('时间筛选不能为空')
+        return false
+      }
+      if (
+        new Date(this.start) > new Date(this.selDate[0]) ||
+        new Date(this.end) < new Date(this.selDate[1])
+      ) {
+        this.$Message.info('时间范围超出，请确认')
+        return false
+      }
+      if (this.format === 'yyyy-MM-dd') {
+        this.selDate = [$_moment(this.selDate[0]), $_moment(this.selDate[1])]
+      }
+      this.$emit('input', this.selDate)
+      this.$emit('on-ok', this.selDate)
+    },
+    // 在 confirm 模式或 clearable = true 时有效，在清空日期时触发
+    handleClear() {
+      const date = ['', '']
+      this.$emit('input', date)
+      this.$emit('on-clear', date)
+    },
+    // 点击外部关闭下拉菜单时触发
+    handleClickoutside() {},
+    // 点击
+    nativeClick() {
+      const target = this.$refs['rd-date-picker-inner'].$refs['pickerPanel']
+      this.$emit('on-click', target)
+    }
+  }
 }
 </script>
