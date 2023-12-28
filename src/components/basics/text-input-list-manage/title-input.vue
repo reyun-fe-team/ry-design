0d5583c98734058dca185674390bc9fbdb982233
杨玉峰 <yangyufeng@mobvista.com>
2023-12-28 19:53:34 +0800

    fix: 删除多行输入的实时输入更新功能，解决中文输入光标重置后无法输入问题

--- src/components/basics/text-input-list-manage/title-input.vue
+++ src/components/basics/text-input-list-manage/title-input.vue
@@ -226,7 +226,7 @@ export default {
           : getPlainText(stringHtml)
       }
       this.saveSelection()
-      this.$emit('input', oiginalText)
+      // this.$emit('input', oiginalText)
       this.$emit('on-change', {
         currentData,
         keyInputEvent: e,
