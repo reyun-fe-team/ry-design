06a24e12e49a1e2a310c97c3a2636515af90a1ae
杨玉峰 <yangyufeng@mobvista.com>
2024-07-25 14:24:30 +0800

    feat: 修改date-picker 可以传入type 支持'daterange', 'datetimerange'

--- src/components/basics/date-picker/date-picker.vue
+++ src/components/basics/date-picker/date-picker.vue
@@ -5,7 +5,7 @@
       ref="rd-date-picker-inner"
       v-model="selDate"
       :style="styles"
-      type="daterange"
+      :type="type"
       :transfer="transfer"
       :confirm="confirm"
       :clearable="clearable"
@@ -32,6 +32,13 @@ import { getKey } from '@src/util/assist'
 export default {
   name: prefixCls,
   props: {
+    type: {
+      // daterange datetimerange
+      default: 'daterange',
+      validator(value) {
+        return ['daterange', 'datetimerange'].includes(value)
+      }
+    },
     value: {
       type: Array,
       default: () => []
