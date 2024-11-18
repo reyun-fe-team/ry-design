ce381fba093c47631b4369c0bca87449e1319e07
杨玉峰 <yangyufeng@mobvista.com>
2024-11-18 18:06:06 +0800

    feat: 提交组件增加回显字段

--- examples/routers/submission-rule.vue
+++ examples/routers/submission-rule.vue
@@ -2,12 +2,29 @@
   <div class="edit-row-bid">
     <rd-submission-rule
       media-code="toutiao"
+      :select-form-data="selectFormData"
       @on-change="handleChange"></rd-submission-rule>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      selectFormData: {
+        adsSubmitRule: 'DELAY',
+        adsDelaySubmitTime: '2024-11-18 17:43',
+        adsBatchStartTime: '2024-11-18 17:43',
+        adsSubmitInterval1: 'FIVE_MINUTE',
+        adsSubmitDimension: 'PLAN',
+        adsMoreAccount: 'OFF',
+        adsSubmitNum1: 1,
+        adsRepeatSubmitTime: '2024-11-18 17:43',
+        adsSubmitInterval2: 'FIVE_MINUTE',
+        adsSubmitNum2: 2
+      }
+    }
+  },
   methods: {
     handleChange(data) {
       console.log('data', data)
